67bd960cff82439a2df2bf754330e12d8260cfa6
Kingsley Victor <javaprodigy56@gmail.com>
2019-11-28 13:35:59 +0100

    initial commit

---
+++ src/test/java/com/jobs/board/JobBoardConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jobs.board;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JobBoardConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
